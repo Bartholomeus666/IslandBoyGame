01ebb35278412ba54d287dc8d0b8950f7fccaca8
Nand Kalhofer <kalhofernand@gmail.com>
2025-03-26 22:07:01 +0100

    Merge with main

--- IslandBoyGame/Assets/C_Scripts/EnemyMovingState.cs
+++ IslandBoyGame/Assets/C_Scripts/EnemyMovingState.cs
@@ -1,85 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 
-public class EnemyMovingState : MonoBehaviour
+public class EnemyMovingState : MonoBehaviour, IState
 {
-    public void Raycasting()
+    private NavMeshAgent agent;
+    public GameObject target;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.destination = target.transform.position;
+    }
+
+    public void OnEnter()
     {
 
     }
+
+    public void OnExit()
+    {
+
+    }
+
+    void IState.Update()
+    {
+        agent.destination = target.transform.position;
+    }
 }
-
-//public class Enemy : MonoBehaviour
-//{
-
-//    private Transform myTransform;
-//    private float speed = 5.0f;
-//    private bool isWalking = true;
-//    private Vector3 curNormal = Vector3.up;
-//    private Vector3 hitNormal = Vector3.zero;
-
-//    void Start()
-//    {
-//        myTransform = transform;
-//    }
-
-//    void Update()
-//    {
-//        switch (isWalking)
-//        {
-//            case true:
-//                // check for wall
-//                RaycastHit rayHit;
-//                if (Physics.Raycast(myTransform.position, myTransform.forward, out rayHit, 1))
-//                {
-//                    hitNormal = rayHit.normal;
-//                    isWalking = false;
-//                }
-//                Debug.DrawRay(myTransform.position, myTransform.forward * 1.0f, Color.red);    // show forward ray    
-
-//                // check for no floor    
-//                Vector3 checkRear = myTransform.position + (-myTransform.forward * 0.25f);
-//                if (Physics.Raycast(checkRear, -myTransform.up, out rayHit, 1))
-//                {
-//                    // there is a floor!
-//                }
-//                else
-//                {
-//                    // find the floor around the corner
-//                    Vector3 checkPos = myTransform.position + (myTransform.forward * 0.5f) + (-myTransform.up * 0.51f);
-//                    Debug.DrawRay(checkPos, -myTransform.forward * 1.5f, Color.green);    // show floor check ray
-//                    if (Physics.Raycast(checkPos, -myTransform.forward, out rayHit, 1))
-//                    {
-//                        Debug.Log("HitNormal " + rayHit.normal);
-//                        hitNormal = rayHit.normal;
-//                        isWalking = false;
-//                    }
-//                }
-//                Debug.DrawRay(myTransform.position, -myTransform.up * 1.0f, Color.red);    // show down ray
-//                // move forward
-//                MoveForward();
-//                break;
-
-//            case false:
-//                curNormal = Vector3.Lerp(curNormal, hitNormal, 4.0f * Time.deltaTime);
-//                Quaternion grndTilt = Quaternion.FromToRotation(Vector3.up, curNormal);
-//                transform.rotation = grndTilt;
-//                float check = (curNormal - hitNormal).sqrMagnitude;
-//                if (check < 0.001f)
-//                {
-//                    grndTilt = Quaternion.FromToRotation(Vector3.up, hitNormal);
-//                    transform.rotation = grndTilt;
-//                    isWalking = true;
-//                }
-//                break;
-//        }
-//    }
-
-//    void MoveForward()
-//    {
-//        myTransform.position += transform.forward * speed * Time.deltaTime;
-//    }
-
-//}
